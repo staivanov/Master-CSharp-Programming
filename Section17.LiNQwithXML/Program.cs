88a9a67467d32cdf1664e739862bedac65ef4314
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-02 22:41:45 +0300

    Linq with XML methods were created.

---
+++ Section17.LiNQwithXML/Program.cs
@@ -0,0 +1,102 @@
+﻿using static System.Console;
+using System.Xml.Linq;
+
+
+namespace Section17.LiNQwithXML
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            WriteLine("Ascending order of the students:");
+            PrintStudents(OrderStudents());
+            WriteLine("--------------------------------");
+            WriteLine("Descending order of the students: ");
+            PrintStudents(OrderStudents(descOrder: true));
+        }
+
+        private static XDocument GetStudentsFromXMLfile()
+        {
+            XDocument studentsXdoc = new();
+            string studentsOnXML = XMLdataSeed.GetXMLdata();
+            studentsXdoc = XDocument.Parse(studentsOnXML);
+
+            return studentsXdoc;
+        }
+
+        private static IEnumerable<dynamic> GetStudents()
+        {
+            XDocument studentsXdoc = GetStudentsFromXMLfile();
+
+            string descendantElement = "Student",
+                   name = "Name",
+                   age = "Age",
+                   university = "University",
+                   semester = "Semester";
+
+            var students = from student
+                           in studentsXdoc.Descendants(descendantElement)
+                           select new
+                           {
+                               Name = student.Element(name)?.Value,
+                               Age = student.Element(age)?.Value,
+                               University = student.Element(university)?.Value,
+                               Semester = student.Element(semester)?.Value
+                           };
+
+            return students;
+        }
+
+        private static void PrintStudents(IEnumerable<dynamic> students)
+        {
+            string message = string.Empty;
+
+            foreach (dynamic student in students)
+            {
+                message = $"Student {student.Name} with age {student.Age} from University {student.University}.";
+                WriteLine(message);
+            }
+        }
+
+        private static IEnumerable<dynamic> OrderStudents(bool descOrder = false)
+        {
+            string descendantElement = "Student",
+                 name = "Name",
+                 age = "Age",
+                 university = "University",
+                 semester = "Semester";
+
+            XDocument studentsXdoc = GetStudentsFromXMLfile();
+            IEnumerable<dynamic> students;
+
+            if (descOrder)
+            {
+                students = from student
+                           in studentsXdoc.Descendants(descendantElement)
+                           orderby student.Element(age)?.Value descending
+                           select new
+                           {
+                               Name = student.Element(name)?.Value,
+                               Age = student.Element(age)?.Value,
+                               University = student.Element(university)?.Value,
+                               Semester = student.Element(semester)?.Value
+                           };
+
+                return students;
+            }
+
+            students = from student
+                       in studentsXdoc.Descendants(descendantElement)
+                       orderby student.Element(age)?.Value ascending
+                       select new
+                       {
+                           Name = student.Element(name)?.Value,
+                           Age = student.Element(age)?.Value,
+                           University = student.Element(university)?.Value,
+                           Semester = student.Element(semester)?.Value
+                       };
+
+            return students;
+        }
+    }
+}
---
+++ Section17.LiNQwithXML/XMLdataSeed.cs
@@ -0,0 +1,38 @@
+﻿namespace Section17.LiNQwithXML
+{   //Fictional XML file 
+    public static class XMLdataSeed
+    {
+        public static string GetXMLdata()
+        {   
+            string studentsXML =
+                       @"<Students>
+                            <Student>
+                                <Name>Toni</Name>
+                                <Age>21</Age>
+                                <University>Yale</University>
+                                <Semester>6</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Carla</Name>
+                                <Age>17</Age>
+                                <University>Yale</University>
+                                <Semester>1</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Leyla</Name>
+                                <Age>19</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>3</Semester>
+                            </Student>
+                            <Student>
+                                <Name>Frank</Name>
+                                <Age>25</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>10</Semester>
+                            </Student>
+                        </Students>";
+
+            return studentsXML;
+        }
+    }
+}
