096648828ac789a338055a11025bba64015ec3f5
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-03 11:49:27 +0300

    New method InsertStudent was created plus DisplayStudentsOnDataTable method.

--- Section10.LinqToSql/Section10.LinqToSql/MainWindow.xaml.cs
+++ Section10.LinqToSql/Section10.LinqToSql/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Configuration;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace Section10.LinqToSql
 {
@@ -17,8 +20,15 @@ namespace Section10.LinqToSql
             string connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
             _dataContext = new LinqToSqlDataClassesDataContext(connectionString);
 
-            InsertUniversity(new University() { Name = "Yale" });
-            DisplayOnDataTable();
+            Student vasko = new Student()
+            {
+                Name = "Vasko",
+                Gender = "Male"
+            };
+            string oxford = "University of Oxford";
+
+            //InsertStudent(vasko, oxford);
+            DisplayStudentsOnDataTable();
         }
 
 
@@ -33,9 +43,24 @@ namespace Section10.LinqToSql
             _dataContext.SubmitChanges();
         }
 
-        private void DisplayOnDataTable()
+
+        private void InsertStudent(Student newStudent, string universityName)
         {
-            MainDataGrid.ItemsSource = _dataContext.Universities;
+            University uni = _dataContext
+                .Universities
+                .FirstOrDefault(u => u.Name == universityName);
+
+            newStudent.UniversityId = uni.Id;
+            newStudent.University = uni;
+
+            _dataContext.Students.InsertOnSubmit(newStudent);
+            _dataContext.SubmitChanges();
         }
+
+        private void DisplayUniversitiesOnDataTable()
+            => MainDataGrid.ItemsSource = _dataContext.Universities;
+
+        private void DisplayStudentsOnDataTable()
+            => MainDataGrid.ItemsSource = _dataContext.Students;
     }
 }
