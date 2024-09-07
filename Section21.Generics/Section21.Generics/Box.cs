e88a34e21a38d0cb477fd97b36e4ffc9f71e9181
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-07 22:43:57 +0300

    Section 21 Generics was init.

---
+++ Section21.Generics/Section21.Generics/Box.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace Section21.Generics
+{
+    public class Box<T>
+    {
+        private T _content;
+
+        public Box(T initialValue)
+        {
+            _content = initialValue;
+        }
+
+
+        public T GetContent()
+        {
+            return _content;
+        }
+
+
+        public void UpdateContent(T newContent)
+        {
+            _content = newContent;
+            WriteLine($"Updated content to {_content}");
+        }
+    }
+}
---
+++ Section21.Generics/Section21.Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace Section21.Generics
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Box<string> boxStr = new Box<string>("Hello World");
+            boxStr.UpdateContent("Teaching C# is fun");
+            WriteLine(boxStr.GetContent());
+
+
+
+
+
+            
+        }
+    }
+}
