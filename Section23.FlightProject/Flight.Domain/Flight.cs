2f381ef8bf763870a2b05c18fb1c9fe4363be3ad
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-11 09:40:25 +0300

    Flight project was created. Domain and test project were init.

---
+++ Section23.FlightProject/Flight.Domain/Flight.cs
@@ -0,0 +1,20 @@
+﻿namespace Flights.Domain
+{
+    public class Flight
+    {
+        public int SeatCapacity { get; set; }
+        public int RemainingNumbersOfFreeSeats { get; set; }
+
+        public Flight(int seatCapacity)
+        {
+            SeatCapacity = seatCapacity;
+            RemainingNumbersOfFreeSeats = seatCapacity;
+        }
+
+
+        public void Book(string v1, int v2)
+        {
+            RemainingNumbersOfFreeSeats -= v2;
+        }
+    }
+}
