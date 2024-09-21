983dceb82ff53dd5c24ea4afed127ab83e7eff0f
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-21 11:35:54 +0300

    First GUI element was created.

--- Section13.WPFapp/WPFapp/MainWindow.xaml.cs
+++ Section13.WPFapp/WPFapp/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFapp
 {
@@ -19,6 +11,17 @@ namespace WPFapp
         public MainWindow()
         {
             InitializeComponent();
+
+            Button myButton = new();
+            myButton.Content = "AB";
+            Grid.SetRow(myButton, 3);
+            Grid.SetColumn(myButton, 4);
+
+            Grid? myGrid = FindName("myGrid") as Grid;
+            myGrid.Children.Add(myButton);
+
+
+
         }
     }
 }
